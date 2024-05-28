cf4014ad820ebe9aff1134d4a4e41f4866308bf0
JisuKlk <alvarofdez.f10@gmail.com>
2024-05-28 12:59:08 +0200

    Auto constructor

---
+++ JavaDatabaseExample/src/main/java/com/example/database/models/Student.java
@@ -0,0 +1,9 @@
+package main.java.com.example.database.models;
+
+public class Student {
+
+    public Student(int i, String string, String string2) {
+        //TODO Auto-generated constructor stub
+    }
+    
+}
