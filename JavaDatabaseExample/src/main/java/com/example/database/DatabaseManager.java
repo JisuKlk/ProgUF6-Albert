3b6b16bb52711d69028d1af52285e66388c060dd
JisuKlk <alvarofdez.f10@gmail.com>
2024-05-28 16:29:22 +0200

    Some changes

--- JavaDatabaseExample/src/main/java/com/example/database/DatabaseManager.java
+++ JavaDatabaseExample/src/main/java/com/example/database/DatabaseManager.java
@@ -2,13 +2,28 @@ package main.java.com.example.database;
 
 import main.java.com.example.database.models.Student;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import main.lib.DatabaseConnection;
 
 public class DatabaseManager {
     private Connection connection;
+
+    public class DatabaseConnection {
+    public static Connection getConnection() {
+        String url = "jdbc:postgresql://localhost:5432/school";
+        String user = "root";
+        String password = "password";
+        
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
 
     // We get the conn
     public void connect() {
